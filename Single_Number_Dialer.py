d7dcf74a5789f2133913446b23652f19f7b4b3ba
Matt Packwood <mattpackwood@gmail.com>
2020-02-03 18:45:45 -0500

    Update Single_Number_Dialer.py

--- Single_Number_Dialer.py
+++ Single_Number_Dialer.py
@@ -2,13 +2,13 @@ import applescript
 
 scpt = applescript.AppleScript('''
     on run {arg1, arg2}
-    say arg1 & " " & arg2activate application "Messages"
-    tell application "System Events" to tell process "Messages"
-    key code 45 using command down           -- press Command + N to start a new window
-    keystroke "+17345856169‬"  -- input the phone number
-    key code 36                              -- press Enter to focus on the message area
-    keystroke "This is a test"       -- type some message
-    key code 36                              -- press Enter to send
-    end tell
+        say arg1 & " " & arg2activate application "Messages"
+        tell application "System Events" to tell process "Messages"
+            key code 45 using command down           -- press Command + N to start a new window
+            keystroke "+17345856169‬"  -- input the phone number
+            key code 36                              -- press Enter to focus on the message area
+            keystroke "This is a test"       -- type some message
+            key code 36                              -- press Enter to send
+        end tell
     end run
 ''')
