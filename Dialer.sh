6ea0c94cd0cadfd1f964f00ffec26de10154958f
Matt Packwood <mattpackwood@gmail.com>
2020-04-17 12:37:06 -0400

    Update Dialer.sh
    Message update

--- Dialer.sh
+++ Dialer.sh
@@ -2,5 +2,5 @@
 for phone_number in `cat $1`
 do
 echo $phone_number
-osascript sendMessage.applescript $phone_number "Hello and good morning, week three of my new job and I have now spent more time working from home than I did in the office"
+osascript sendMessage.applescript $phone_number "Hello there, just checking in how you are doing in the current unpleasantness?"
 done
