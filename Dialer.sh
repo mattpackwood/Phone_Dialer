8636b7de741164f8cbcf1fa964f0e1c5abb66acb
Matt Packwood <mattpackwood@gmail.com>
2020-05-08 10:51:17 -0400

    Added comments

--- Dialer.sh
+++ Dialer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+#Loop through all numbers in text file and execut the AppleScript I wrote to send the messaged
 for phone_number in `cat $1`
-do
-echo $phone_number
-osascript sendMessage.applescript $phone_number "Hi, still working from the home office in Rochester Hills, how are you doing?"
-done
+  do
+    echo $phone_number
+    osascript sendMessage.applescript $phone_number "Hi, still working from the home office in Rochester Hills, how are you doing?"
+  done
