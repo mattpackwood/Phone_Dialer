30d4a43ed0f8e8070de841c648fec947c5b4877c
Matt Packwood <mattpackwood@gmail.com>
2020-09-23 21:10:39 -0400

    update

--- Dialer.sh
+++ Dialer.sh
@@ -4,5 +4,5 @@
 for phone_number in `cat $1`
   do
     echo $phone_number
-    osascript sendMessage.applescript $phone_number "Good morning, how are you doing today?"
+    osascript sendMessage.applescript $phone_number "Hi, still working from the home office in Rochester Hills, how are you doing?"
   done
